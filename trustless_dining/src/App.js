01ac8456d483a747d89d3f01dcad5bc9669da161
shenae <shhnae@gmail.com>
2019-07-21 00:24:32 -0400

    set up pages and routes

--- trustless_dining/src/App.js
+++ trustless_dining/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar';
 import Process from './components/Process';
+import Demo from './components/Demo';
+import Docs from './components/Docs';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
-     <Process /> 
+    <main>
+    <Route exact path="/Demo" component={Demo}/>
+    <Route exact path="/Process" component={Process}/>
+    <Route exact path="/Docs" component={Docs}/>
+    </main>
     </div>
   );
 }
---
+++ trustless_dining/src/components/Docs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Docs extends Component {
+  render() {
+    return (
+    <div>
+        
+    </div>
+
+        );
+  }
+}
+
+export default Docs;
