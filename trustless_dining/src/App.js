c7cc037e5f2cc7f9a3335ba2b662f0ca5f935d6b
shenae <shhnae@gmail.com>
2019-07-20 21:39:05 -0400

    created pages and added flowchart pic

--- trustless_dining/src/App.js
+++ trustless_dining/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './App.css';
 import NavBar from './components/NavBar';
+import Process from './components/Process';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
-      
+     <Process /> 
     </div>
   );
 }
--- trustless_dining/src/components/Process.js
+++ trustless_dining/src/components/Process.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import flowchart from '../assets/VerifyWithBrightID.png';
 
 class Process extends Component {
   render() {
     return (
     <div>
-        
+      <img src={flowchart} alt="flowchart of process" />
     </div>
 
         );
